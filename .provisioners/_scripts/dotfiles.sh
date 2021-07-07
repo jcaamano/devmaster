97b731e8231354a58501f835d1183f3e8ae138ab
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2021-07-07 12:58:53 +0000

    Add upstream repo for dotfiles

--- .provisioners/_scripts/dotfiles.sh
+++ .provisioners/_scripts/dotfiles.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-repo="git@github.com:jcaamano/dotfiles.git"
+repo="jcaamano/dotfiles.git"
+repo_https="https://github.com/$repo"
+repo_git="git@github.com:$repo"
 
 [ -d "${HOME}/.dotfiles" ] && {
     cd ${HOME}/.dotfiles
-    git pull -f --depth 1 "$repo"
+    git pull -f --depth 1 "$repo_https"
 } || {
-    git clone --depth 1 "$repo" ~/.dotfiles
+    git clone --depth 1 "$repo_https" ~/.dotfiles
+    git remote add upstream "$repo_git"
 }
 
 cd ${HOME}/.dotfiles
