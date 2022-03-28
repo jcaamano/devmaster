d2464d93396fde7ca65459d385fda6a66cb3ae70
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2022-03-28 16:59:20 +0200

    Fix golang installer
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- .provisioners/_scripts/golang.sh
+++ .provisioners/_scripts/golang.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 version="$(curl -L 'https://golang.org/VERSION?m=text')"
 
@@ -6,8 +6,9 @@ wget "https://dl.google.com/go/${version}.linux-amd64.tar.gz"
 rm -rf /usr/local/go && tar -C /usr/local -xzf ${version}.linux-amd64.tar.gz
 rm -f ${version}.linux-amd64.tar.gz
 
-mkdir -p ~/.zshrc.d/
-rm -f ~/.zshrc.d/golang.zsh
-echo "path=(/usr/local/go \$path)" > ~/.zshrc.d/openshift.zsh
-echo "export PATH" >> ~/.zshrc.d/openshift.zsh
-source ~/.zshrc.d/openshift.zsh
+CONFIG="/home/vagrant/.zshrc.d/golang.zsh"
+mkdir -p $(basename $CONFIG)
+rm -f $CONFIG
+echo "path=(/usr/local/go/bin \$path)" > $CONFIG
+echo "export PATH" >> $CONFIG
+source $CONFIG
