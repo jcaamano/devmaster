40f4c0f1ba1d01028bfc8d6d8dfd135eebdb3135
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2022-03-28 17:00:14 +0200

    Fix openshift installer
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- .provisioners/_scripts/openshift.sh
+++ .provisioners/_scripts/openshift.sh
@@ -1,19 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 VERSION=${1:-latest}
-URL="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}"
+URL="${URL:-https://mirror.openshift.com/pub/openshift-v4/clients/ocp}"
 DIR="/home/$USER/src/openshift/installer/${VERSION}"
 
 mkdir -p /home/$USER/src/openshift/installer/${VERSION}
 mkdir -p "$DIR" && cd "$DIR"
-wget "${URL}/openshift-client-linux.tar.gz" && tar -xzvf openshift-client-linux.tar.gz
-wget "${URL}/openshift-install-linux.tar.gz" && tar -xzvf openshift-install-linux.tar.gz
-rm openshift-install-linux.tar.gz openshift-client-linux.tar.gz
+wget "${URL}/${VERSION}/openshift-client-linux-${VERSION}.tar.gz" && tar -xzvf openshift-client-linux-${VERSION}.tar.gz
+wget "${URL}/${VERSION}/openshift-install-linux-${VERSION}.tar.gz" && tar -xzvf openshift-install-linux-${VERSION}.tar.gz
+rm openshift-install-linux-${VERSION}.tar.gz openshift-client-linux-${VERSION}.tar.gz
 
-mkdir -p ~/.zshrc.d/
-[ -f "~/.zshrc.d/openshift.zsh" ] && rm -f ~/.zshrc.d/openshift.zsh
-echo "path=($DIR \$path)" > ~/.zshrc.d/openshift.zsh
-echo "export PATH" >> ~/.zshrc.d/openshift.zsh
-echo "export GOOGLE_CREDENTIALS=\${HOME}/dev/openshift/shared-secrets/gce/aos-serviceaccount.json" >> ~/.zshrc.d/openshift.zsh
-source ~/.zshrc.d/openshift.zsh
+CONFIG="/home/vagrant/.zshrc.d/openshift.zsh"
+mkdir -p $(basename $CONFIG)
+rm -f $CONFIG
+echo "path=($DIR \$path)" > $CONFIG
+echo "export PATH" >> $CONFIG
+echo "export GOOGLE_CREDENTIALS=\${HOME}/dev/openshift/shared-secrets/gce/aos-serviceaccount.json" >> $CONFIG
+source $CONFIG
 
