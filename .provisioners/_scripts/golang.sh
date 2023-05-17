32c4ecd07f54461893ff082bb5859658dfa95bd8
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2023-05-17 11:42:43 +0200

    add ~/go/bin to PATH
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- .provisioners/_scripts/golang.sh
+++ .provisioners/_scripts/golang.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# install go
 version="$(curl -L 'https://golang.org/VERSION?m=text')"
 
 wget "https://dl.google.com/go/${version}.linux-amd64.tar.gz"
@@ -9,7 +10,7 @@ rm -f ${version}.linux-amd64.tar.gz
 CONFIG="/home/vagrant/.zshrc.d/golang.zsh"
 mkdir -p $(dirname $CONFIG)
 rm -f $CONFIG
-echo "path=(/usr/local/go/bin \$path)" > $CONFIG
+echo "path=(/home/vagrant/go/bin /usr/local/go/bin \$path)" > $CONFIG
 echo "export PATH" >> $CONFIG
 chown -R vagrant:vagrant $(dirname $CONFIG)
 source $CONFIG
